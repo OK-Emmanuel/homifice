d68454f3137f613c41e7b75d08fbbb280bcb7d06
Olawuni Emmanuel Kayode <rexkay1234@gmail.com>
2024-07-12 16:19:12 +0100

    Production environment configurations

--- hms/settings.py
+++ hms/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rigezp_&wp7=f+-ahv@6(p&i8c7u^5r7%e4%v7r&ny!$zx!(^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
 
